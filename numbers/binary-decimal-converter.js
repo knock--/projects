eb1f01bc199567cc5075ade74fcace4a22153347
knock-- <root@black-hand.com>
2015-03-18 18:20:47 -0500

    Add solution for binary/decimal converter

---
+++ numbers/binary-decimal-converter.js
@@ -0,0 +1,34 @@
+(function () {
+    "use strict";
+
+    var num = prompt(),
+        convertFromBinary = true,
+        conversion = i = 0,
+        base = 2,
+        convertNumeral = function () {
+            return num[num.length - i - 1] * Math.pow(base, i);
+        };
+
+    if (!/^\d+$/.test(num)) {
+        console.log("Please enter a binary or decimal number.");
+        return;
+    }
+
+    if (/[2-9]/.test(num)) {
+        convertFromBinary = false;
+        base = 10;
+    }
+
+    if (convertFromBinary) {
+        for (i = 0; i < num.length; i++) {
+            conversion += convertNumeral();
+        }
+    } else {
+        for (i = 0; i < num.length; i++) {
+            conversion += convertNumeral();
+        }
+
+    }
+
+    console.log(conversion);
+}());
